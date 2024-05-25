9feb79a9a3d50626ef362c270003b69568afec54
Ashok <ashokitschool@gmail.com>
2024-05-25 18:12:15 +0530

    request ann added

--- src/main/java/in/ashokit/rest/MsgRestController.java
+++ src/main/java/in/ashokit/rest/MsgRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import in.ashokit.service.MsgService;
 
 @RestController
+@RequestMapping("/api")
 public class MsgRestController {
 	
 	@Autowired
